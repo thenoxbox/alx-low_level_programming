7c174974ea54ec0a742239fbe0926b12ae71feee
thenoxbox <lordyam307@gmail.com>
2022-12-09 07:42:37 -0800

    Additional files

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *  * main - main block
+ *   * Return: 1 do not go well
+ *    */
+int main(void)
+{
+
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+						  59);
+		return (1);
+}
