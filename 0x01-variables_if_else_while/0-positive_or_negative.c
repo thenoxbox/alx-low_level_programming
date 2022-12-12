38b11cdb40edadbe89bf912ac5853ecc10fdb1d9
thenoxbox <lordyam307@gmail.com>
2022-12-12 13:19:40 -0800

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ *
+ * Return: all good
+ */
+int main(void)
+{	
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n).
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print statements based on the last digit of the random number.
+ * Return: all good
+ */
+int main(void)
+{
+	
+	int n; /*Variable declaration*/
+	int m; /**/
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	m = n % 10;
+
+					
+	if (m > 5)
+		printf("Last digit of %i is %i and is greater than 5\n",
+			   n, m);
+	else if (m == 0)
+		printf("Last digit of %i is %i and is 0\n", n, m);
+	else
+		printf("Last digit of %i is %i and is less than 6 and not 0\n",
+		           n, m);
+
+	return (0);
+}
