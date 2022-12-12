f3827994ba425ec0ed0e3ff25726c5cadaca444d
Chukwuemeka Oluchukwu <lordyam307@gmail.com>
2022-12-12 22:27:25 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
- *  * main - main block
- *   * Description: Print all numbers of base 10, starting from 0.
- *    * Return: All good
- *     */
+ * main - main block
+ * Description: Print all numbers of base 10, starting from 0.
+ * Return: All good
+ */
 int main(void)
 {
-		int i;
+	int i;
 
-			for (i = 0; i < 10; i++)
-					{
-								putchar(i + '0');
-									}
-				putchar('\n');
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
 
-					return (0);
+	return (0);
 }
