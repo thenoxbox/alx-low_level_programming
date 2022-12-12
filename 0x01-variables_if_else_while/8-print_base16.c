fad67f9b1188f9e1296eeb91c549965bc7094513
Chukwuemeka Oluchukwu <lordyam307@gmail.com>
2022-12-12 22:30:02 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 /**
- *  * main - main block
- *   * Description: Print all numbers of base64 in lowercase.
- *    * Return: All good
- *     */
+ * main - main block
+ * Description: Print all numbers of base64 in lowercase.
+ * Return: All good
+ */
 int main(void)
 {
-		char c;
-			int i;
+	char c;
+	int i;
 
-				for (i = 0; i < 10; i++)
-						{
-									putchar(i + '0');
-										}
-					for (c = 'a'; c < 'g'; c++)
-							{
-										putchar(c);
-											}
-						putchar('\n');
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (c = 'a'; c < 'g'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
 
-							return (0);
+	return (0);
 }
