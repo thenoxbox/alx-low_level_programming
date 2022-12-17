8dcf650e2e6f06e86fcad08a83d16330c72d594b
Chukwuemeka Oluchukwu <lordyam307@gmail.com>
2022-12-17 14:48:27 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,18 @@
 #include "main.h"
 /**
- * main - prints out _putchar
- * Return: All good
+ * main - prints _putchar, fllowd by a new linees
+ * Return: Alwys 0 (Success)
  */
 int main(void)
 {
-	char *s = "_putchar"; /*arrays of characters or strings*/
-	int i = 0;
+	char abe[] = "_putchar";
 
-	while (s[i] != '\0')
+	int c;
+
+	for (c = 0; c < 8; c++)
 	{
-		_putchar(s[i]);
-		i++;
+		_putchar(abe[c]);
 	}
 	_putchar('\n');
-
 	return (0);
 }
