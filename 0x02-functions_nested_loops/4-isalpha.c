4f8fd302f77d6b44128c6176a5f6adf03f4a7264
Chukwuemeka Oluchukwu <lordyam307@gmail.com>
2022-12-17 14:47:44 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,23 @@
 #include "main.h"
+
 /**
- * _isalpha - Check if character is an alphabet.
- * @c: type int character
- * Return: 1 if letter, lowercase or uppercase, and 0 otherwise
+ * _isalpha - check the code for ALX School students.
+ *
+ * @c: is c an integer argument
+ *
+ * Return: Always 0.
  */
+
 int _isalpha(int c)
-{	
-	/*if c is between 64 and 91 ASCII decimals for chars or 96 and 123 */
-	if ((c > 64 && c < 91) || (c > 96 && c < 123))
+{
+
+	if (c >= 97 && c <= 122)
+	{
 		return (1);
-	else
-		return (0);
+	}
+	else if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
 }
